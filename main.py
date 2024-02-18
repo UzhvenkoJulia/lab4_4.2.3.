5e30de68da8857ca59079b6830503350e81fb06c
UzhvenkoJulia <144743928+UzhvenkoJulia@users.noreply.github.com>
2024-02-18 22:00:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from utils import is_power_of_five
+from utils import is_power_of_five, is_power_of_two
 
 def main():
-    # виклик ф is_power_of_five з utils.py
-    number = 125
+    number = 16
     if is_power_of_five(number):
         print(f"{number} є степенем 5")
     else:
         print(f"{number} не є степенем 5")
+
+    if is_power_of_two(number):
+        print(f"{number} є степенем 2")
+    else:
+        print(f"{number} не є степенем 2")
 
 if __name__ == "__main__":
     main()
