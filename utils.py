4f5805f449e61901858b07cabd400730d23a7b5e
UzhvenkoJulia <144743928+UzhvenkoJulia@users.noreply.github.com>
2024-02-18 21:31:09 +0200

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,19 @@
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+def is_prime(num):
+    if num <= 1:
+        return False
+    elif num <= 3:
+        return True
+    elif num % 2 == 0 or num % 3 == 0:
+        return False
+    i = 5
+    while i * i <= num:
+        if num % i == 0 or num % (i + 2) == 0:
+            return False
+        i += 6
+    return True
